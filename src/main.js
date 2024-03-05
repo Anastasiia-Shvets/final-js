6010cc3c80af8c4cc57888fab673f436364ef126
Anastasiia-Shvets <144601223+Anastasiia-Shvets@users.noreply.github.com>
2024-03-05 02:18:31 +0200

    push

--- src/main.js
+++ src/main.js
@@ -2,15 +2,28 @@ const themeSwitch = document.getElementById('theme-switch');
 themeSwitch.addEventListener('change', function () {
   if (this.checked) {
     document.body.classList.add('dark-theme');
-    // localStorage.setItem('theme', 'dark');
+    localStorage.setItem('theme', 'dark');
   } else {
     document.body.classList.remove('dark-theme');
-    // localStorage.setItem('theme', 'light');
+    localStorage.setItem('theme', 'light');
   }
 });
 
-// const savedTheme = localStorage.getItem('theme');
-// if (savedTheme === 'dark') {
-//   themeSwitch.checked = true;
-//   document.body.classList.add('dark-theme');
-// }
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+  themeSwitch.checked = true;
+  document.body.classList.add('dark-theme');
+}
+
+
+
+// const theme = document.getElementById('icon-logotip');
+// const logo = document.getElementById('icon-dark-logo');
+
+// theme.addEventListener('change', () => {
+//     if (theme.checked) {
+//         logo.src = '../img/noptimizesprite.svg#icon-dark-logo';
+//     } else {
+//         logo.src = './img/noptimizesprite.svg#icon-logotip';
+//     }
+// });
